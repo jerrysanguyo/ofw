730debeb9db71686f7dcdc137ac8693e0ed864cc
jerrysanguyo <jsanguyo1624@gmail.com>
2024-06-07 15:59:15 +0800

    minor

--- resources/views/Report/index.blade.php
+++ resources/views/Report/index.blade.php
@@ -5,41 +5,7 @@
         <div class="col-lg-4">
             <div class="card shadow border">
                 <div class="card-body">
-                    <span class="fs-4"><i class="fa-solid fa-handshake m-3"></i>Beneficiary count</span>
-                    <div class="row align-items-end mt-4">
-                        <div class="col-md-4">
-                            <label for="startAge" class="form-label">Age from:</label>
-                            <select name="startAge" id="startAge" class="form-select">
-                                @php
-                                    for($i=1; $i<=100; $i++) {
-                                        echo '<option value="' . $i . '">' . $i . ' Years old</option>';
-                                    }
-                                @endphp
-                            </select>
-                        </div>
-                        <div class="col-md-4">
-                            <label for="endAge" class="form-label">To:</label>
-                            <select name="endAge" id="endAge" class="form-select">
-                                @php
-                                    for($i=1; $i<=100; $i++) {
-                                        echo '<option value="' . $i . '">' . $i . ' Years old</option>';
-                                    }
-                                @endphp
-                            </select>
-                        </div>
-                        <div class="col-md-4 d-flex justify-content-md-start">
-                            <button id="ageCountBtn" class="btn btn-primary mt-4 mt-md-0">Generate</button>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row text-center m-3">
-                        <span id="AgeCount" class="fs-1"></span>
-                    </div>
-                    <div class="d-grid gap-2 text-center">
-                        <a href="#">
-                            <button class="btn btn-primary">Generate Excel</button>
-                        </a>
-                    </div>
+                    @include('Report/Partial/ageReport')
                 </div>
             </div>
         </div>
